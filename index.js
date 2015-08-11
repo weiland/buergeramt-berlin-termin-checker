2845271bbc5d9f59cc28b7c09211c5c5c30fe061
Pascal Weiland <pasweiland@gmail.com>
2015-08-11 19:51:04 +0200

    Initial commit :octocat:

---
+++ index.js
@@ -0,0 +1,79 @@
+var got = require('got');
+var cheerio = require('cheerio');
+
+const URL = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister%5B%5D=122210&dienstleister%5B%5D=122217&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&anliegen%5B%5D=120686&herkunft=%2Fterminvereinbarung%2F';
+//const URL = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?id=&buergerID=&buergername=&absagecode=&Datum=1435701600&anliegen%5B%5D=120686&dienstleister%5B%5D=122210&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&herkunft='
+const SELECTOR = 'td.buchbar';
+const DESIRED_MONTH = 'august';
+
+function notify(ms) {
+  var push = require( 'pushover-notifications' );
+
+  var p = new push( config );
+
+  var msg = {
+      // These values correspond to the parameters detailed on https://pushover.net/api
+      // 'message' is required. All other values are optional.
+    message: 'just found a Termin for the Anmeldung der Wohnung: ' + ms,   // required
+      title: 'Found Appointment',
+      sound: 'magic',
+      device: 'devicename',
+      url: 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister%5B%5D=122210&dienstleister%5B%5D=122217&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&anliegen%5B%5D=120686&herkunft=%2Fterminvereinbarung%2F',
+      priority: 1
+  };
+
+  p.send( msg, function( err, result ) {
+      if ( err ) {
+        console.log('errorororrror');
+          throw err;
+      }
+
+      console.log( 'resutl', result );
+  });
+}
+
+function getMonth(td) {
+  var ret;
+  try {
+    ret = td.parent.parent.prev.prev.children[1].children[2].next.children[0].data.trim().replace(/ 2015/, '');
+  } catch(e) {
+    console.log(parentElement);
+  }
+  return ret;
+}
+
+function getDay(parentElement) {
+  var ret;
+  try {
+    ret = parentElement.children[0].children[0].children[0].children[0].data;
+  } catch(e) {
+    console.log(parentElement);
+  }
+  return ret;
+}
+
+function isDesiredMonth(month) {
+  return month.toLowerCase() === DESIRED_MONTH;
+}
+
+function handleData(data) {
+  $ = cheerio.load(data);
+  var freeDays = $(SELECTOR);
+  console.log('Found: ', freeDays.length);
+  if(freeDays.length > 0) {
+    var i,l;
+    for(i=0,l=freeDays.length; l>i; i++) {
+      var month = getMonth(freeDays[i]);
+      var day = getDay(freeDays[i]);
+      var isDesired = isDesiredMonth(month);
+      if (isDesired) notify(freeDays[0]);
+      console.log(day, month, isDesired);
+    }
+  }
+}
+
+got(URL, function handleResponse(err, data) {
+    if(err) console.error('Error: ', err);
+    handleData(data);
+});
+
